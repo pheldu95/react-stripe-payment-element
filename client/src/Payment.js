1e41bbfc7186e7eedbf57b230f700e316abd97c8
Duncan Phelps <phelps.duncan@gmail.com>
2022-10-09 14:51:05 -0600

    built out payment.js

--- client/src/Payment.js
+++ client/src/Payment.js
@@ -1,6 +1,14 @@
 import { useEffect, useState } from "react";
-
+import {loadStripe} from "@stripe/stripe-js";
 function Payment(props) {
+  const[stripePromise, setStripePromise] = useState(null);
+  const [clientSecret, setClientSecret] = useState("");
+  useEffect(() =>{
+    fetch("/config").then(async(r)=>{
+      const {publishableKey} = await r.json();
+      setStripePromise(loadStripe(publishableKey));
+    })
+  }, [])
   return (
     <>
       <h1>React Stripe and the Payment Element</h1>
